a52af9bcbd159f3ab0de68189637963e3b2d6190
Ionut-SV <volintiru.ionut@yahoo.com>
2025-01-04 14:26:29 +0200

    images upload update

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const bookController = require('../controllers/bookController');
-
+const path = require('path');
 // Multer setup for file uploads
 const storage = multer.diskStorage({
-    destination: './uploads/',
+    destination: path.join(__dirname, 'uploads'), 
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
     }
 });
-const upload = multer({ storage });
+const upload = multer({ storage }); 
 
 // Define routes
 router.post('/', upload.single('image'), bookController.addBook);
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const db = require('./config/db');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Test database connection
 db.getConnection((err) => {
@@ -31,6 +31,7 @@ db.getConnection((err) => {
 // Routes
 app.use('/auth', authRoutes);
 app.use('/books', bookRoutes);
+app.use('/uploads', express.static('uploads'));
 
 // Global error handler
 app.use((err, req, res, next) => {
